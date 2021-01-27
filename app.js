5445c9eb9ba474542ff8d871c3be55561ae8ba8c
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2021-01-27 21:24:26 +0530

    init

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const request = require("request");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use((req,res,next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     next();
-})
+});
 
 app.get("/", (req, res, next) => {
-    const url = "http://newsapi.org/v2/sources?language=en&apiKey=8a23bbf32e024b5b98c24ebfd1cfb188"
-    request(url).pipe(res)
+    const url =
+        "http://newsapi.org/v2/sources?language=en&apiKey=8a23bbf32e024b5b98c24ebfd1cfb188";
+    request(url).pipe(res);
+});
+app.get("/search/:query", (req, res, next) => {
+    const query = req.params.query;
+    const url = `http://newsapi.org/v2/everything?q=${query}&apiKey=8a23bbf32e024b5b98c24ebfd1cfb188`;
+    request(url).pipe(res);
 });
 
 app.listen(PORT, () => {
